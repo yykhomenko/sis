f6d41d730886430042955a6cd7b7d8853082eaa9
yykhomenko <yykhomenko@gmail.com>
2021-12-16 12:31:51 +0200

    added base store

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,8 +1,37 @@
 package store
 
-import "github.com/yykhomenko/sis/internal/entity"
+import (
+	"errors"
+
+	"github.com/yykhomenko/sis/internal/entity"
+)
+
+var ErrNotExist = errors.New("info not exist")
 
 type Store interface {
 	Get(int64) (*entity.Info, error)
 	Set(info entity.Info) error
+}
+
+type database struct {
+	infos map[int64]entity.Info
+}
+
+func NewStore() Store {
+	return &database{
+		infos: make(map[int64]entity.Info),
+	}
+}
+
+func (db *database) Get(msisdn int64) (*entity.Info, error) {
+	info, exist := db.infos[msisdn]
+	if !exist {
+		return nil, ErrNotExist
+	}
+	return &info, nil
+}
+
+func (db *database) Set(info entity.Info) error {
+	db.infos[info.Msisdn] = info
+	return nil
 }
