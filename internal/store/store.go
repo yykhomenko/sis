b8a611f315ae63773547d405ddf4213c11bc1747
yykhomenko <yykhomenko@gmail.com>
2021-12-16 12:47:26 +0200

    added pointers

--- internal/store/store.go
+++ internal/store/store.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"errors"
+	"time"
 
 	"github.com/yykhomenko/sis/internal/entity"
 )
@@ -10,16 +11,16 @@ var ErrNotExist = errors.New("info not exist")
 
 type Store interface {
 	Get(int64) (*entity.Info, error)
-	Set(info entity.Info) error
+	Set(info *entity.Info) error
 }
 
 type database struct {
-	infos map[int64]entity.Info
+	infos map[int64]*entity.Info
 }
 
 func NewStore() Store {
 	return &database{
-		infos: make(map[int64]entity.Info),
+		infos: make(map[int64]*entity.Info),
 	}
 }
 
@@ -28,10 +29,11 @@ func (db *database) Get(msisdn int64) (*entity.Info, error) {
 	if !exist {
 		return nil, ErrNotExist
 	}
-	return &info, nil
+	return info, nil
 }
 
-func (db *database) Set(info entity.Info) error {
+func (db *database) Set(info *entity.Info) error {
+	info.ChangeDate = time.Now()
 	db.infos[info.Msisdn] = info
 	return nil
 }
